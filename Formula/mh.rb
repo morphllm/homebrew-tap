c979d1b452158a5ad903a62a186375e860e29e46
bhaktatejas922 <bhaktatejas922@gmail.com>
2026-03-21 18:35:11 -0700

    Update mh v0.1.0 with OAuth support

--- Formula/mh.rb
+++ Formula/mh.rb
@@ -6,26 +6,25 @@ class Mh < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://code.morphllm.com/attachments/7a68466a-71a7-4ca4-a54e-fa8724d1c652"
-      sha256 "2574da678eb5ee47b811aa2ab3544fd0605700f92331b64812a9ca65870a25a6"
+      url "https://code.morphllm.com/attachments/6b29b558-c6ac-408e-ae52-6720fd9e188b"
+      sha256 "e15a1a83dc19027dd5963bc5627a95eae3c54f0ba36a61aa4ae6ca79429fa690"
     else
-      url "https://code.morphllm.com/attachments/e0a40cec-77fc-496f-859e-cec78c12d6e8"
-      sha256 "09def0587f57bd0e9c70b7e3fd9536f60cab24770857c057a48a48697f5d3ffb"
+      url "https://code.morphllm.com/attachments/9fc543b2-728c-4a4c-9dc4-61d28d4c93f1"
+      sha256 "30693aa41d705c78885bcaf4ba0a4fc7bd21b1a66e8739f88759ccf9c0db726f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://code.morphllm.com/attachments/f9226dec-9017-4cb9-a994-aad340d8f8c6"
-      sha256 "65163f52cd7bc0f774a2f0b0aea6db3360fc61dbca62db3ae41f531720ae8c8a"
+      url "https://code.morphllm.com/attachments/b3c17728-2f61-4f85-a44b-49e2a1036dea"
+      sha256 "80a4c34e4a484efb023273004856057d5202e4ec3987dbf62b59caf97ac420e1"
     else
-      url "https://code.morphllm.com/attachments/11f8e7d7-1a9d-4b63-9eed-c70d04d552dc"
-      sha256 "2a61d2806922ad5c0ef62df3253f8dd82c9f2a41a05bb25228968b1bcbfb5564"
+      url "https://code.morphllm.com/attachments/ca2f4f02-9d8a-4de9-b635-b0306b99c46f"
+      sha256 "bc1bf4007947c3e727b668c5eb738fec2ceedd55489985f35a5e4a6b5c04437f"
     end
   end
 
   def install
-    # Downloaded file has no extension, just install as mh
     bin.install Dir["*"].first => "mh"
   end
 
