aae645ac937b45fc3c46d0723b6a6a6f97c34a8c
bhaktatejas922 <bhaktatejas922@gmail.com>
2026-03-21 18:49:54 -0700

    Use release download URLs instead of attachment UUIDs

--- Formula/mh.rb
+++ Formula/mh.rb
@@ -6,20 +6,20 @@ class Mh < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://code.morphllm.com/attachments/6b29b558-c6ac-408e-ae52-6720fd9e188b"
+      url "https://code.morphllm.com/bhaktateas/cli/releases/download/v0.1.0/mh-darwin-arm64"
       sha256 "e15a1a83dc19027dd5963bc5627a95eae3c54f0ba36a61aa4ae6ca79429fa690"
     else
-      url "https://code.morphllm.com/attachments/9fc543b2-728c-4a4c-9dc4-61d28d4c93f1"
+      url "https://code.morphllm.com/bhaktateas/cli/releases/download/v0.1.0/mh-darwin-amd64"
       sha256 "30693aa41d705c78885bcaf4ba0a4fc7bd21b1a66e8739f88759ccf9c0db726f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://code.morphllm.com/attachments/b3c17728-2f61-4f85-a44b-49e2a1036dea"
+      url "https://code.morphllm.com/bhaktateas/cli/releases/download/v0.1.0/mh-linux-arm64"
       sha256 "80a4c34e4a484efb023273004856057d5202e4ec3987dbf62b59caf97ac420e1"
     else
-      url "https://code.morphllm.com/attachments/ca2f4f02-9d8a-4de9-b635-b0306b99c46f"
+      url "https://code.morphllm.com/bhaktateas/cli/releases/download/v0.1.0/mh-linux-amd64"
       sha256 "bc1bf4007947c3e727b668c5eb738fec2ceedd55489985f35a5e4a6b5c04437f"
     end
   end
